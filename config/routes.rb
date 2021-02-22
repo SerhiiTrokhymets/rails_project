b605581e08140f6f6c6f93be61d5cae3c9508870
SerhiiTrokhymets <serheyfly4410@gmail.com>
2021-02-22 21:42:40 +0200

    update_controllers_views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/posts' => 'posts#index'
-  get '/posts/:id' => 'posts#show'
+  # get '/users' => 'users#index'
+  # get 'users/new' => 'users#new'
+  # get '/users/:id' => 'users#show'
+  # get 'users/:id/posts/new' => 'posts#new'
+  # get '/users/:id/posts' => 'posts#index'
+  # get 'users/:id/posts/:id' => 'posts#show'
+  # post 'users' => 'users#create'
+  # post 'posts' => 'posts#create'
+  resources :users do
+    resources :posts, shallow: true
+  end
   mount Blog::API => '/'
 end
